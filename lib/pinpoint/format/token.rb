e16ffcc4ca151c03d3ec9183b87372fa192646dc
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-13 15:52:53 -0600

    Make sure that Token can be instantiated properly

--- lib/pinpoint/format/token.rb
+++ lib/pinpoint/format/token.rb
@@ -3,6 +3,11 @@ require 'ostruct'
 module Pinpoint
   module Format
     class Token < Struct.new(:type, :value)
+      def initialize(*args)
+        args[0] = args[0].to_sym
+
+        super
+      end
     end
   end
 end
--- spec/format/token_spec.rb
+++ spec/format/token_spec.rb
@@ -2,4 +2,17 @@ require 'rspectacular'
 require 'pinpoint/format/token'
 
 describe Pinpoint::Format::Token do
+  it 'can be instantiated with the proper arguments' do
+    token = Pinpoint::Format::Token.new(:foo, 'bar')
+
+    token.type.should   eql :foo
+    token.value.should  eql 'bar'
+  end
+
+  it 'always has a symbol for a type even if instantiated with something else' do
+    token = Pinpoint::Format::Token.new('foo', 'bar')
+
+    token.type.should   eql :foo
+    token.value.should  eql 'bar'
+  end
 end

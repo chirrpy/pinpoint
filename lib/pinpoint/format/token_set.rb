21dee0c6401a58888dedc5fb8dca357e660b5bda
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-13 15:06:15 -0600

    Add TokenSet

---
+++ lib/pinpoint/format/token_set.rb
@@ -0,0 +1,6 @@
+module Pinpoint
+  module Format
+    class TokenSet < Array
+    end
+  end
+end
---
+++ spec/format/token_set_spec.rb
@@ -0,0 +1,8 @@
+require 'rspectacular'
+require 'pinpoint/format/token_set'
+
+describe Pinpoint::Format::TokenSet do
+  it 'is true' do
+    true
+  end
+end

bd8d070346ca7edee9b015693e3893391197e6ae
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-13 15:17:36 -0600

    Add TokenSet.valid?

--- lib/pinpoint/format/token_set.rb
+++ lib/pinpoint/format/token_set.rb
@@ -1,6 +1,30 @@
+require 'pinpoint/format/parse_error'
+
 module Pinpoint
   module Format
     class TokenSet < Array
+      ##
+      # Public: Verifies that the tokens in the list are in good form.
+      #
+      # Returns TrueClass if the tokens in the list are valid
+      # Raises Pinpoint::Format::UnevenNestingError if the number of
+      #   'group_start' tokens does not match the number of 'group_end' tokens.
+      #
+      def valid?
+        raise Pinpoint::Format::UnevenNestingError if group_start_count != group_end_count
+
+        true
+      end
+
+      private
+
+      def group_start_count
+        count { |token| token[0] == :group_start }
+      end
+
+      def group_end_count
+        count { |token| token[0] == :group_end }
+      end
     end
   end
 end
--- spec/format/token_set_spec.rb
+++ spec/format/token_set_spec.rb
@@ -6,4 +6,16 @@ describe Pinpoint::Format::TokenSet do
   let(:token_set)       { token_set_class.new }
 
   it 'is an Array' do; token_set.should be_an Array; end
+
+  it 'is valid if the number of group pair tokens is equal' do
+    token_set = token_set_class.new [[:group_start], [:group_end]]
+
+    token_set.should be_valid
+  end
+
+  it 'is not valid if the number of group pair tokens is unequal' do
+    token_set = token_set_class.new [[:group_start]]
+
+    expect { token_set.valid? }.to raise_error Pinpoint::Format::UnevenNestingError
+  end
 end

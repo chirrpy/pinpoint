f267171f5418224ea3a3d1e8b1694932e0778bc0
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-13 23:10:55 -0600

    Add Style#output which outputs an address in a given format

--- lib/pinpoint/format/style.rb
+++ lib/pinpoint/format/style.rb
@@ -3,6 +3,7 @@ require 'pinpoint/format/parser'
 module Pinpoint
   module Format
     class Style
+
       ##
       # Public: Processes the style information gleaned from the Pinpoint YAML
       # definition format and generates a Style from it.
@@ -18,6 +19,55 @@ module Pinpoint
         style = self.new
         style.send(:structure=, Format::Parser.new(style_definition).parse)
         style
+      end
+
+      ##
+      # Public: Can take an address-like object and output it in the style
+      # specified by the structure.
+      #
+      # Example
+      #
+      #   # Assuming address is an address-like object and Style was
+      #   # instantiated with '(%s, )((%l, )(%p )%z(, %c)'
+      #   output address
+      #   # => '123 First Street, Nashville, TN 37033, United States'
+      #
+      def output(address)
+        process(structure, address)
+      end
+
+      protected
+
+      ##
+      # Protected: Access to the underlying structure of the Style (typically is
+      # a result of calling Pinpoint::Format::Parser#parse.
+      #
+      attr_accessor :structure
+
+      private
+
+      ##
+      # Private: Can take a Style structure and assemble a String from data from
+      # a context.
+      #
+      # When finishing with a grouping, if there is no alphanumeric content
+      # within a grouping, it will be discarded.
+      #
+      # Returns a String containing data from the context and String literals
+      #
+      def process(structure, context)
+        processed_structure = structure.each_with_object('') do |token, result|
+          result << case token
+                    when Array
+                      process(token, context)
+                    when Symbol
+                      context.public_send(token)
+                    when String
+                      token
+                    end
+        end
+
+        processed_structure.match(/\A[^A-Za-z0-9]*\z/) ? '' : processed_structure
       end
     end
   end
--- spec/format/style_spec.rb
+++ spec/format/style_spec.rb
@@ -1,15 +1,33 @@
 require 'rspectacular'
 require 'pinpoint/format/style'
-require 'awesome_print'
 
 describe Pinpoint::Format::Style do
   let(:style_class) { Pinpoint::Format::Style }
 
   it 'can instantiate itself based on a style definition' do
     style_definition = '((%s, )(%l, ))(%p )%z(, %c)'
-
-    style = style_class.from_definition_yaml style_definition
+    style            = style_class.from_definition_yaml style_definition
 
     style.send(:structure).first.first.should eql [:street, ', ']
+  end
+
+  it 'can output an address based on a style' do
+    style_definition = '((%s, )(%l, ))(%p )%z(, %c)'
+    style            = style_class.from_definition_yaml style_definition
+    address          = stub(street:       'a',
+                            locality:     'b',
+                            province:     'c',
+                            postal_code:  'd',
+                            country:      'e')
+
+    style.output(address).should eql 'a, b, c d, e'
+  end
+
+  it 'understands not to output a grouping without alphanumeric characters' do
+    style_definition = '(%s, )'
+    style            = style_class.from_definition_yaml style_definition
+    address          = stub(street: '')
+
+    style.output(address).should eql ''
   end
 end

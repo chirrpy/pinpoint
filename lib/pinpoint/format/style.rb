8bfacb083a3468cb9d56fc1ae8eeb62ddfaa24e2
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-13 20:07:11 -0600

    Add Style.from_definition_yaml
    Horrible name but it's good enough for now

--- lib/pinpoint/format/style.rb
+++ lib/pinpoint/format/style.rb
@@ -1,6 +1,24 @@
+require 'pinpoint/format/parser'
+
 module Pinpoint
   module Format
     class Style
+      ##
+      # Public: Processes the style information gleaned from the Pinpoint YAML
+      # definition format and generates a Style from it.
+      #
+      # style - The style information from the Pinpoint YAML definition file.
+      #         For example:
+      #
+      #             (%s, )((%l, )(%p )%z(, %c)
+      #
+      # Returns a Pinpoint::Format::Style based on the information passed in.
+      #
+      def self.from_definition_yaml(style_definition)
+        style = self.new
+        style.send(:structure=, Format::Parser.new(style_definition).parse)
+        style
+      end
     end
   end
 end
--- spec/format/style_spec.rb
+++ spec/format/style_spec.rb
@@ -1,8 +1,15 @@
 require 'rspectacular'
 require 'pinpoint/format/style'
+require 'awesome_print'
 
 describe Pinpoint::Format::Style do
-  it 'is true' do
-    true
+  let(:style_class) { Pinpoint::Format::Style }
+
+  it 'can instantiate itself based on a style definition' do
+    style_definition = '((%s, )(%l, ))(%p )%z(, %c)'
+
+    style = style_class.from_definition_yaml style_definition
+
+    style.send(:structure).first.first.should eql [:street, ', ']
   end
 end

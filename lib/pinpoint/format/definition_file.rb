e4a9ebc73a63a4f1acd0da2d76d9b26f3bf5c408
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-15 14:48:54 -0600

    Add DefinitionFile

---
+++ lib/pinpoint/format/definition_file.rb
@@ -0,0 +1,6 @@
+module Pinpoint
+  module Format
+    class DefinitionFile
+    end
+  end
+end
---
+++ spec/format/definition_file_spec.rb
@@ -0,0 +1,8 @@
+require 'rspectacular'
+require 'pinpoint/format/definition_file'
+
+describe Pinpoint::Format::DefinitionFile do
+  it 'is true' do
+    true
+  end
+end

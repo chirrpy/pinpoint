300ae5dadcd24daae2d0d843e393d1b24fefa368
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-15 19:01:25 -0600

    Add AddressFormatter

--- lib/pinpoint/address_formatter.rb
+++ lib/pinpoint/address_formatter.rb
@@ -1,2 +1,84 @@
+require 'pinpoint/format/definition_set'
+
 class Pinpoint::AddressFormatter
+
+  ##
+  # Public: Is able to process an Address into numerous formats based on the
+  # country and style, or a completely custom format.
+  #
+  # address - An Object that responds to #name, #street, #city, #state, #county,
+  #           #country, #zip_code and #country.
+  # options - A Hash of options describing how the address should be formatted
+  #
+  #           :country - Each country displays its addresses in a specific
+  #                      format. Pinpoint knows about these. Passing in two
+  #                      letter ISO code as a Symbol for a country will use that
+  #                      country's format. (defaults to :us)
+  #           :style   - Can be a Symbol which relates to any style in the
+  #                      definition file that is loaded. Default formats are:
+  #
+  #                        * :one_line
+  #                        * :one_line_with_name
+  #                        * :multi_line
+  #                        * :multi_line_with_name
+  #                        * :html
+  #
+  #                      All but the 'html' style is standard text.
+  #
+  #                      The :html option wraps each piece of the address as
+  #                      well as the entire address in HTML tags to which CSS
+  #                      can be applied for either a single-line or multi-line
+  #                      layout.
+  #
+  #                      Additionally :html will append classes to the different
+  #                      pieces of the address which correspond to both the
+  #                      globally generic name (eg 'locality').
+  #
+  # Example
+  #
+  #   format(address, :country => :us, :style => :one_line)
+  #   # => 'Kwik-E-Mart, 123 Apu Lane, Springfield, NW 12345, United States'
+  #
+  #
+  #   format(address, :country => :us, :style => :multi_line)
+  #   # => 'Kwik-E-Mart
+  #   #     123 Apu Lane
+  #   #     Springfield, NW 12345
+  #   #     United States'
+  #
+  #
+  #   format(address, country:  :us,
+  #                   style:    :html)
+  #
+  #   # => '<address>
+  #   #       <span class="section">
+  #   #         <span class="name">Kwik-E-Mart</span>
+  #   #       </span>
+  #   #       <span class="section">
+  #   #         <span class="street">123 Apu Lane</span>
+  #   #       </span>
+  #   #       <span class="section">
+  #   #         <span class="city locality">Springfield</span>
+  #   #         <span class="state state_or_province">NW</span>
+  #   #         <span class="zip_code postal_code">12345</span>
+  #   #       </span>
+  #   #       <span class="section">
+  #   #         <span class="country">United States</span>
+  #   #       </span>
+  #   #     </address>'
+  #
+  def self.format(address, options = {})
+    country       = options.fetch(:country, :us)
+    style         = options.fetch(:style,   :one_line)
+
+    definition    = definitions[country]
+
+    definition.output address, :style => style
+  end
+
+  private
+
+  def self.definitions
+    @definitions ||= Pinpoint::Format::DefinitionSet.new
+  end
 end
--- spec/address_formatter_spec.rb
+++ spec/address_formatter_spec.rb
@@ -1,5 +1,112 @@
 require 'rspectacular'
+require 'ostruct'
 require 'pinpoint/address_formatter'
 
+
+class Address < Struct.new( :name,
+                            :street,
+                            :locality,
+                            :province,
+                            :county,
+                            :postal_code,
+                            :country,
+                            :latitude,
+                            :longitude)
+end
+
 describe Pinpoint::AddressFormatter do
+  let(:address) { Address.new 'Kwik-E-Mart',
+                              '123 Apu Lane',
+                              'Springfield',
+                              'NW',
+                              'Springfield County',
+                              '12345',
+                              'United States',
+                              '12345',
+                              '67890' }
+
+  describe '#format' do
+    it 'can format it in one line US format' do
+      expected = "Kwik-E-Mart, 123 Apu Lane, Springfield, NW 12345, United States"
+
+      Pinpoint::AddressFormatter.format(address, country: :us,
+                                                 style:   :one_line_with_name)
+                                .should eql(expected)
+    end
+
+    it 'can format it in multiline US format' do
+      expected = <<-MULTILINE
+Kwik-E-Mart
+123 Apu Lane
+Springfield, NW 12345
+United States
+      MULTILINE
+
+      Pinpoint::AddressFormatter.format(address, country: :us,
+                                                 style:   :multi_line_with_name)
+                                .should eql(expected)
+    end
+
+    it 'can format it in HTML in US format' do
+      expected = <<-MULTILINE
+<address>
+  <span class="section">
+    <span class="name">Kwik-E-Mart</span>
+  </span>
+  <span class="section">
+    <span class="street">123 Apu Lane</span>
+  </span>
+  <span class="section">
+    <span class="locality">Springfield</span>
+    <span class="province">NW</span>
+    <span class="postal_code">12345</span>
+  </span>
+  <span class="section">
+    <span class="country">United States</span>
+  </span>
+</address>
+      MULTILINE
+
+      Pinpoint::AddressFormatter.format(address, country: :us,
+                                                 style:   :html)
+                                .should eql(expected)
+    end
+
+    context 'when the address contains unsafe characters' do
+      let(:address) { Address.new "<script>alert('Gotcha!');</script>",
+                                  '123 Apu Lane',
+                                  'Springfield',
+                                  'NW',
+                                  'Springfield County',
+                                  '12345',
+                                  'United States',
+                                  '12345',
+                                  '67890' }
+
+      it 'escapes anything that is unsafe' do
+        expected = <<-MULTILINE
+<address>
+  <span class="section">
+    <span class="name">&lt;script&gt;alert(&#x27;Gotcha!&#x27;);&lt;/script&gt;</span>
+  </span>
+  <span class="section">
+    <span class="street">123 Apu Lane</span>
+  </span>
+  <span class="section">
+    <span class="locality">Springfield</span>
+    <span class="province">NW</span>
+    <span class="postal_code">12345</span>
+  </span>
+  <span class="section">
+    <span class="country">United States</span>
+  </span>
+</address>
+        MULTILINE
+
+        Pinpoint::AddressFormatter.format(address, country: :us,
+                                                  style:   :html)
+                                  .should eql(expected)
+      end
+    end
+  end
 end

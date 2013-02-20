37811f3f8879fbc187d84afbca8b80b3e62eade8
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-20 13:22:42 -0600

    Add Mapable#map_url to create a Google Maps URL when called

--- lib/pinpoint/mapable.rb
+++ lib/pinpoint/mapable.rb
@@ -1,5 +1,14 @@
+require 'cgi'
+
 module Pinpoint
   module Mapable
+    def map_url(options = {})
+      map_address          = self.to_s.to_str
+      escaped_map_address  = CGI.escape map_address
+      address_name         = self.name
+      escaped_address_name = CGI.escape address_name
 
+      %Q{http://maps.google.com?q=#{escaped_map_address} (#{escaped_address_name})}
+    end
   end
 end
--- spec/mapable_spec.rb
+++ spec/mapable_spec.rb
@@ -1,8 +1,32 @@
 require 'rspectacular'
 require 'pinpoint/mapable'
+require 'pinpoint/address'
+require 'awesome_print'
+
+class MyMapableClass < Pinpoint::Address
+end
 
 describe Pinpoint::Mapable do
-  it 'is true' do
-    true
+  let(:address) { MyMapableClass.new name:                 'name with \\stuff/',
+                                     street_and_premises:  'street and premises',
+                                     city:                 'city',
+                                     state:                'state',
+                                     county:               'county',
+                                     postal_code:          'postal code',
+                                     country:              'country',
+                                     latitude:             'latitude',
+                                     longitude:            'longitude' }
+
+  context 'when it is mixed in to a Pinpoint::Address-like class' do
+    it 'can create a Google Maps URL from the address' do
+      address.map_url(:via => :google_maps).should eql 'http://maps.google.com?q=' +
+                                                       'street+and+premises%2C+' +
+                                                       'city%2C+' +
+                                                       'state+' +
+                                                       'postal+code%2C+' +
+                                                       'country' +
+                                                       ' ' +
+                                                       '(name+with+%5Cstuff%2F)'
+    end
   end
 end
